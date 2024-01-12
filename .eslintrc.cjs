aa77be39dea62e80dea4670b82eece457c6e3887
Lucas Dang <dhp371.work@gmail.com>
2024-01-12 18:17:42 +0700

    chore: add prettier and lint fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,30 +1,30 @@
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
   ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["prettier", "@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['prettier', '@typescript-eslint'],
   rules: {
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
         singleQuote: true,
       },
     ],
   },
   ignorePatterns: [
-    "*.js",
-    "*.cjs",
-    "package.json",
-    "package-lock.json",
-    "pnpm-lock.yaml",
-    "pnpm-workspace.yaml",
-    "dist",
-    ".sst",
-    "/**/node_modules/*",
-    "*.d.ts",
+    '*.js',
+    '*.cjs',
+    'package.json',
+    'package-lock.json',
+    'pnpm-lock.yaml',
+    'pnpm-workspace.yaml',
+    'dist',
+    '.sst',
+    '/**/node_modules/*',
+    '*.d.ts',
   ],
 };
