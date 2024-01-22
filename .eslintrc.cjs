16daba0bfb5430397a43fa936bc687334e342755
arahiko-ayami <zinang7@gmail.com>
2024-01-22 23:41:05 +0700

    refactor: code quality set up

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,30 +1,34 @@
 module.exports = {
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "plugin:prettier/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["prettier", "@typescript-eslint"],
-  rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        singleQuote: true,
-      },
-    ],
-  },
-  ignorePatterns: [
-    "*.js",
-    "*.cjs",
-    "package.json",
-    "package-lock.json",
-    "pnpm-lock.yaml",
-    "pnpm-workspace.yaml",
-    "dist",
-    ".sst",
-    "/**/node_modules/*",
-    "*.d.ts",
-  ],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+		'plugin:prettier/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	plugins: ['prettier', '@typescript-eslint'],
+	rules: {
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				trailingComma: 'all',
+				tabWidth: 2,
+				insertPragma: false,
+				endOfLine: 'lf',
+			},
+		],
+	},
+	ignorePatterns: [
+		'*.js',
+		'*.cjs',
+		'package.json',
+		'package-lock.json',
+		'pnpm-lock.yaml',
+		'pnpm-workspace.yaml',
+		'dist',
+		'.sst',
+		'/**/node_modules/*',
+		'*.d.ts',
+	],
 };
